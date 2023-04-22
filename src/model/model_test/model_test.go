93c181ee1c7a616f555a9517cb534a44ee87a93f
bit-dream <61420422+bit-dream@users.noreply.github.com>
2023-04-22 17:07:30 -0400

    Re-arranged folder structure. Added utility methods for virtual model. Added some test cases

---
+++ src/model/model_test/model_test.go
@@ -0,0 +1,31 @@
+package model_test
+
+import (
+	"github.com/bit-dream/go-virtual/src/model"
+	"testing"
+)
+
+var GlobalTestVm, _ = model.LoadModel("test.network")
+
+func TestModelLoad(t *testing.T) {
+
+	_, err := model.LoadModel("test.network")
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestBadFileExtension(t *testing.T) {
+
+	_, err := model.LoadModel("test.json")
+	if err == nil {
+		t.Error("LoadModel function did not return an error for inappropriate file type extension")
+	}
+}
+
+func TestGroupMessagesByChannel(t *testing.T) {
+	mapping := model.GetMessagesByChannels(*GlobalTestVm)
+	if mapping == nil {
+		t.Error("Model returned nil channels")
+	}
+}
