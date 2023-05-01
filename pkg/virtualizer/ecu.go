d592846dddcd1d1a7eb53c7250085454408828df
bit-dream <61420422+bit-dream@users.noreply.github.com>
2023-04-30 20:08:58 -0400

    Modified functions so that it will actually run

--- pkg/virtualizer/ecu.go
+++ pkg/virtualizer/ecu.go
@@ -14,14 +14,20 @@ func VirtualizeEcu(ecu ecu_model.VirtualEcu, stop chan bool) error {
 		rate := message.TransmissionOptions.TransmitRate
 
 		dbcData := *ecu.DbcData
-		dbcSignals, ok := dbcData[message.Name]
+		dbcMessage, ok := dbcData[message.Name]
 		if !ok {
 			return fmt.Errorf("could not find message %s in DBC data", message.Name)
 		}
 
-		signals := GenerateCompleteSignalList(dbcSignals, message)
+		signals := ConvertDbcMessageToVirtualSignals(dbcMessage, message)
+		/*
+			if message.Signals != nil {
+				OverrideVirtualSignals(signals, *message.Signals)
+			}
+		*/
+		signalList := getAllValues(signals)
 		if signals == nil || len(signals) == 0 {
-			return fmt.Errorf("signals returned nil in VirtualizeEcu")
+			return fmt.Errorf("an error occured while generating signal list to be sent by the virtual ecu")
 		}
 
 		if rate != nil {
@@ -30,7 +36,7 @@ func VirtualizeEcu(ecu ecu_model.VirtualEcu, stop chan bool) error {
 					fmt.Println("Error encountered were signals were nil: ", message)
 					return
 				}
-				payload := can_database.GeneratePayloadFromSignals(signals)
+				payload := can_database.GeneratePayloadFromSignals(signalList)
 				fmt.Println(payload)
 			})
 		} else {
@@ -39,6 +45,47 @@ func VirtualizeEcu(ecu ecu_model.VirtualEcu, stop chan bool) error {
 	}
 
 	return nil
+}
+
+func getAllValues(m map[string]ecu_model.VirtualSignal) []ecu_model.VirtualSignal {
+	values := make([]ecu_model.VirtualSignal, 0, len(m))
+	for _, v := range m {
+		values = append(values, v)
+	}
+	return values
+}
+
+func ConvertDbcMessageToVirtualSignals(message descriptor.Message, virtualMessage ecu_model.VirtualMessage) map[string]ecu_model.VirtualSignal {
+
+	var defaultValue int = 0
+	if virtualMessage.DefaultValueForSignals != nil {
+		defaultValue = *virtualMessage.DefaultValueForSignals
+	}
+
+	virtualSignals := make(map[string]ecu_model.VirtualSignal)
+	for _, signal := range message.Signals {
+		_, keyAlreadyExists := virtualSignals[signal.Name]
+		if keyAlreadyExists {
+			continue
+		}
+		newSignal := ecu_model.VirtualSignal{
+			Name:             signal.Name,
+			DefaultValue:     defaultValue,
+			SignalDefinition: signal,
+		}
+		virtualSignals[signal.Name] = newSignal
+	}
+
+	return virtualSignals
+}
+
+func OverrideVirtualSignals(signals map[string]ecu_model.VirtualSignal, overrideSignals []ecu_model.VirtualSignal) {
+	for _, overrideSignal := range overrideSignals {
+		_, hasSignal := signals[overrideSignal.Name]
+		if hasSignal {
+			signals[overrideSignal.Name] = overrideSignal
+		}
+	}
 }
 
 // GenerateCompleteSignalList will consolidate signals that are in the dbc file and signals defined by the Virtual ECU
